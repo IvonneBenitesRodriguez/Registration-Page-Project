9ef59b4869a38ec42c1e7eaef48177a7f09e6066
Ivonne <ivonnebenites2015@gmail.com>
2021-08-19 20:35:55 -0500

    Creando campos de password e ids y clases en HTML, dando color al body , usando funciones, manipulando el Dom , usando prompt y variables en JavaScript

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-let question=prompt("Hola, como estas?😃");
-//let answer= question;
 
-/*if( answer==="bien"){
-    return "Me alegra 😃";
-} else{
-    return "Espero todo se resuelva para ti 😳";
-}*/
+
+    let nameUser = prompt("What is your name? 👩‍💻🧚‍♀");
+    let ageRequest = prompt("What is your age?");
+   
+        if( ageRequest > 18){
+            alert(`Welcome to our classroom ${nameUser} 🌻🌸🌼 hope you enjoy the time with us 😊`);
+        }else{
+            alert(`Sorry ${nameUser} you are too young to start this class 🌩☂😪`);
+        }
+
+   function search(event){
+       let searchInput = document.querySelector("#pass");
+       alert(`Your secret password is ${searchInput.value} 👈`);
+   }
+ let form=document.querySelector(".campos");
+ form.addEventListener("submit",search);
